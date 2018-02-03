83f3e97eb1fbe273e74c5d85bc735ded5a64b4e0
kouuuki <mark.web.888@gmail.com>
2018-02-03 23:16:59 +0900

    deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # devise_for :users,
   #   controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   resources :users
-  resources :home, only: [:index, :show]
   root "users#index"
 end
