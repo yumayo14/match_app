b97afc4234eb34b86b9aff85c7ad2a11c46dcd43
コミットしました <yumayo14@gmail.com>
2018-02-11 21:02:23 +0900

    Pay.jpの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
+require 'payjp'
+
 class UsersController < ApplicationController
+
+  before_action :authenticate_user!
   def index
   end
 
   def show
-  end
+
+    Payjp.api_key = PAYJP_SECRET_KEY
+    customer = Payjp::Customer.retrieve(id: current_user.id.to_s)
+    @customer_cards = customer.cards.data
+   end
+
 
   def edit
   end
@@ -19,4 +28,32 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  def card_registrate
+    Payjp.api_key = PAYJP_SECRET_KEY
+    if (customer = Payjp::Customer.retrieve(id: current_user.id.to_s))
+      customer.card =  params['payjp-token']
+      customer.save
+    else
+      customer = Payjp::Customer.create(
+        id: current_user.id,
+        email: current_user.email,
+        card: params['payjp-token']
+      )
+    end
+  end
+
+  def card_destroy
+    Payjp.api_key = PAYJP_SECRET_KEY
+    customer = Payjp::Customer.retrieve(params[:customer])
+    customer_card = customer.cards.retrieve(params[:id])
+    customer_card.delete
+    redirect_to user_path(current_user.id)
+  end
+
+  private
+  # def card_params
+  #   params.permit(:number,:cvc, :year, :month, :name)
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
-Rails.application.routes.draw do
   devise_for :users
+  root "users#index"
   # devise_for :users,
   #   controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
-  resources :users
-  root "users#index"
+  resources :users do
+    collection do
+      post 'card_registrate'
+      delete 'card_destroy'
+    end
+  end
 end
