7a7fdb32697507285f2032bbf29d15b5774a7676
kosuke-ito <keree.it.owl.uk@gmail.com>
2018-02-14 18:24:48 +0900

    facebook.login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#index"
-  # devise_for :users,
-  #   controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   resources :users do
     collection do
       post 'card_registrate'
